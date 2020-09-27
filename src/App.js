fa19e4f952809a9fb00ad4a63a6409dcf80f9336
luisfernado28 <luisfer_2810@outlook.com>
2020-09-27 18:26:23 -0400

    Header Created

--- src/App.js
+++ src/App.js
@@ -9,12 +9,7 @@ function App() {
   return (
     <div>
       <Header />
-      <Alert dismissible variant="danger">
-            <Alert.Heading>Oh snap! You got an error!</Alert.Heading>
-            <p>
-                Change this and that and try again.
-            </p>
-        </Alert>
+      
       <HellowWorld name= "Luis"/>
       {/* < Counter /> */}
     </div>
