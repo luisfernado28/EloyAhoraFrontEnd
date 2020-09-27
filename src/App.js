18bed7947d1ae05722b1d5e9e807e6bc40de3a92
luisfernado28 <luisfer_2810@outlook.com>
2020-09-27 17:58:17 -0400

    Cleaned App

--- src/App.js
+++ src/App.js
@@ -4,11 +4,17 @@ import HellowWorld from './Components/HelloWorld'
 import Header from './Components/Header'
 
 
+
 function App() {
   return (
     <div>
       <Header />
-
+      <Alert dismissible variant="danger">
+            <Alert.Heading>Oh snap! You got an error!</Alert.Heading>
+            <p>
+                Change this and that and try again.
+            </p>
+        </Alert>
       <HellowWorld name= "Luis"/>
       {/* < Counter /> */}
     </div>
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,11 +1,15 @@
 import React from 'react'
+import { Alert } from 'react-bootstrap';
 
 
-function Header(){
+function Header() {
     return (
-        <header>
-
-        </header>    
+        <Alert dismissible variant="danger">
+            <Alert.Heading>Oh snap! You got an error!</Alert.Heading>
+            <p>
+                Change this and that and try again.
+            </p>
+        </Alert>
     )
 }
 export default Header
